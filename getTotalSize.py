b5bfdbb658b4d0eeaa8fe9cf9df11d56049dcd40
Jason Perr <trezero@gmail.com>
2024-08-21 11:47:44 -0700

    faster

--- getTotalSize.py
+++ getTotalSize.py
@@ -1,13 +1,38 @@
 import os
 import sys
+import concurrent.futures
+import multiprocessing
+
+def get_size(file_path):
+    try:
+        return os.path.getsize(file_path)
+    except (OSError, FileNotFoundError):
+        return 0
+
+def process_directory(directory):
+    total_size = 0
+    for entry in os.scandir(directory):
+        if entry.is_file(follow_symlinks=False):
+            total_size += entry.stat().st_size
+        elif entry.is_dir(follow_symlinks=False):
+            total_size += process_directory(entry.path)
+    return total_size
 
 def get_directory_size(path):
+    cpu_count = multiprocessing.cpu_count()
     total_size = 0
-    for dirpath, dirnames, filenames in os.walk(path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            if not os.path.islink(fp):
-                total_size += os.path.getsize(fp)
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count) as executor:
+        futures = []
+        for entry in os.scandir(path):
+            if entry.is_dir(follow_symlinks=False):
+                futures.append(executor.submit(process_directory, entry.path))
+            elif entry.is_file(follow_symlinks=False):
+                total_size += entry.stat().st_size
+
+        for future in concurrent.futures.as_completed(futures):
+            total_size += future.result()
+
     return total_size
 
 def bytes_to_gb(bytes_value):
